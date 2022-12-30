4ad1da30d19004ca60dfe0b510c2c741c85de442
Chinna Babu Korlagunta <119917273+chinnababu99@users.noreply.github.com>
2022-12-30 17:13:41 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def chinna(request):
+    return render(request,'chinna.html')
+
+
+def ravi(request):
+    return render(request,'ravi.html')
